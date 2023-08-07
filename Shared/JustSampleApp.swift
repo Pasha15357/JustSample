3ff4228931187ab99f415025c004415ee36f5775
Pasha15357 <gavrilovpasha27@gmail.com>
2023-08-07 14:52:10 +0300

    Initial Commit

---
+++ Shared/JustSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustSampleApp.swift
+//  Shared
+//
+//  Created by Паша on 7.08.23.
+//
+
+import SwiftUI
+
+@main
+struct JustSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
