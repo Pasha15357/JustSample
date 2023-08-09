03c4f518e71addd048889d4dd1f3d97ee9247884
Pasha15357 <gavrilovpasha27@gmail.com>
2023-08-09 15:32:48 +0300

    color picker, toggle, color changing, etc

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,24 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     @State var isOn = false
+    @State var color = Color.primary
     
     var body: some View {
         VStack {
+            Toggle("On/off", isOn: $isOn)
+            ColorPicker("Select a color", selection: $color)
+            Image(systemName: isOn ? "battery.100" : "battery.25")
+                .font(.system(size: 150))
+                .foregroundColor(color)
+            Text("Ого, он меняет цвет 😱")
+                .font(.largeTitle)
+                .foregroundColor(color)
             Circle()
                 .frame(maxHeight: 200)
                 .foregroundColor(isOn ? .purple : .mint)
-                .shadow(color: isOn ? .indigo : .orange, radius: 20)
-                .scaleEffect( isOn ? 2 : 1.5)
+                .shadow(color: isOn ? .black : .blue, radius: 20)
+                .scaleEffect( isOn ? 1.2 : 1)
                 .animation(.spring(response: 1, dampingFraction: 1, blendDuration: 1), value: isOn)
             Button("Press Me") {
                 isOn.toggle()
             }
         }
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portrait)
     }
 }
